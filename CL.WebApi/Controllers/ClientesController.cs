6a50ab42a4fcce25e0302b1dd374fd148a71984c
Rubens Lima <rubens.souzalima@hotmail.com>
2024-03-07 19:29:15 -0300

    Connecting database

---
+++ CL.WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,62 @@
+﻿using CL.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<Cliente>()
+            {
+                new Cliente
+                {
+                    Id = 1,
+                    Nome = "João do Caminhão",
+                    DataNascimento = new DateTime(1980, 01, 01)
+                },
+                new Cliente
+                {
+                    Id = 2,
+                    Nome = "Maria do Caminhão",
+                    DataNascimento = new DateTime(1990, 01, 01)
+                }
+            });
+        }
+
+        
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- CL.WebApi/Startup.cs
+++ CL.WebApi/Startup.cs
@@ -1,11 +1,14 @@
+using CL.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,19 +25,35 @@ namespace CL.WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ClContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ClConnection")));
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultório Legal", Version = "v1" });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("./swagger/v1/swagger.json", "CL V1");
+            });
 
             app.UseHttpsRedirection();
 
